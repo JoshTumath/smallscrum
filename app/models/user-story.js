9ddbf86dff5d8388d051fd01d052cd307bd0417c
Josh Tumath <josh@joshtumath.uk>
2016-03-10 01:29:42 +0000

    Model for user story

--- app/models/user-story.js
+++ app/models/user-story.js
@@ -2,6 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  slug: DS.attr('string'),
-  userStories: DS.hasMany('user-story')
+  project: DS.belongsTo('project')
 });
