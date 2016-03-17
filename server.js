02a587102a2f85ce0ab0b6bf695b8b1e04077aa6
Josh Tumath <josh@joshtumath.uk>
2016-03-17 01:56:41 +0000

    Create initial express.js server
    As explained the following guide:
    http://www.programwitherik.com/setup-your-ember-project-with-node/

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var routes = require('./app/routes');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// Configuration ///////////////////////////////////////////////////////////////
+// The location from which the Ember application will be served
+app.use(express.static(__dirname + '/public'));
+
+// Routes //////////////////////////////////////////////////////////////////////
+routes(app);
+
+// Listen //////////////////////////////////////////////////////////////////////
+app.listen(port);
+
+module.exports = app;
