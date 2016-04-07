e7aa96955f3b7a2368fcde3c689e41e39ed3a5b7
Josh Tumath <josh@joshtumath.uk>
2016-04-07 22:18:57 +0100

    Update functionality

--- app/components/ss-project-form.js
+++ app/components/ss-project-form.js
@@ -4,6 +4,21 @@ export default Ember.Component.extend({
   store: Ember.inject.service(),
 
   isFormShowing: false,
+  project: null,
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    // If the component has a model passed into it, then this form is for
+    // editing a new project rather than creating one
+    if (this.get('project')) {
+      // We must make a copy of the model being passed into the component so
+      // that data will not be shared
+      this.set('name', this.get('project.name'));
+    } else {
+      this.set('project', null);
+    }
+  },
 
   actions: {
     showForm() {
@@ -14,10 +29,28 @@ export default Ember.Component.extend({
       this.set('isFormShowing', false);
     },
 
-    create() {
-      this.get('store').createRecord('project', {
-        name: this.get('name')
-      });
+    submit() {
+      if (this.get('project') === null) {
+        // Create
+        let record = this.get('store').createRecord('project', {
+          name: this.get('name')
+        });
+
+        record.save()
+        .then((foo) => {// TODO: Mirage routes for saving. Finish transitionTo
+          this.hideForm();
+        });
+      } else {
+        // Edit
+        this.get('store').findRecord('project', this.get('project.id'))
+        .then((record) => {
+          record.set('name', this.get('name'));
+
+          return record.save();
+        }).then(() => {
+          this.hideForm();
+        });
+      }
     }
   }
 });
